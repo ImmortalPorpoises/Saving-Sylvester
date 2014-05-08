8572f7b94ea7a3a14726d9b037c756661fc39f5c
jmerten14 <standfast300@gmail.com>
2014-05-08 11:15:36 -0500

    tweaked the text a bit for logical reasons in the first room

--- src/com/immortalporpoises/savingsylvester/Desk.java
+++ src/com/immortalporpoises/savingsylvester/Desk.java
@@ -7,7 +7,7 @@ public class Desk extends Thing
 	public Desk()
 	{
 		name = "desk";
-		description = "Scratch sheets of paper, old Whataburger receipts, and post-it notes cover the worn wooden surface of your desk. You are about to look more closely at it, but then you remember that a person just appeared in your room for no reason. You return your attention to the woman.";
+		description = "Scratch sheets of paper, old Whataburger receipts, and post-it notes cover the worn wooden surface of your desk.";
 		collectable = false;
 	}
 }
