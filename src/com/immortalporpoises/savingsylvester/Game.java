dd01c01cde41ec415a176e61f541daac0966aa48
jmerten14 <standfast300@gmail.com>
2014-05-16 08:34:24 -0500

    fixed quotation mark issue

--- src/com/immortalporpoises/savingsylvester/Game.java
+++ src/com/immortalporpoises/savingsylvester/Game.java
@@ -50,12 +50,12 @@ public class Game{
 		}
 		
 		display.setOutput("Saving Sylvester, Copyright 2014 Immortal Porpoises \n\nNote: please limit commands to 2 words, "
-				+ "i.e. �look room,� �examine bear,� �take bear,� �enter door,� etc. \nTo view your inventory, simply "
-				+ "type �view inventory.� To get help, type �help me.� Case and punctuation do not matter."
-				+ "\n\n��and so Sylvester breathed his last. \n The End.� \n\n    We open at the close."
-				+ "\n    You snap the book shut�it�s the latest work by your favorite fantasy author, P.F. Tollers�and"
-				+ " wipe a tear away. �Why did Sylvester have to die?� you wonder out loud. �He was the best "
-				+ "character in the series.� This thought overcomes you, and you break down in pitiful sobs."
+				+ "i.e. \"look room,\" \"examine bear,\" \"take bear,\" \"enter door,\" etc. \nTo view your inventory, simply "
+				+ "type \"view inventory.\" To get help, type \"help me.\" Case and punctuation do not matter."
+				+ "\n\n\"\"and so Sylvester breathed his last. \n The End.\" \n\n    We open at the close."
+				+ "\n    You snap the book shut\"it\"s the latest work by your favorite fantasy author, P.F. Tollers\"and"
+				+ " wipe a tear away. \"Why did Sylvester have to die?\" you wonder out loud. \"He was the best "
+				+ "character in the series.\" This thought overcomes you, and you break down in pitiful sobs."
 				+ "\n    Time passes, and a few hours later, you are in your room, sobbing violently in the fetal position"
 				+ " and holding a giant pink fluffy teddy bear. You throw the teddy bear across the room, walk "
 				+ "intently to your computer and promptly begin whining to the world through various social "
