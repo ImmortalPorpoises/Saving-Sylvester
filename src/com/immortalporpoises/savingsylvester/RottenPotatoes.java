a272f2454660691f613f524092369579edf681fd
jmerten14 <standfast300@gmail.com>
2014-05-16 08:50:05 -0500

    working on adding levels

---
+++ src/com/immortalporpoises/savingsylvester/RottenPotatoes.java
@@ -0,0 +1,11 @@
+package com.immortalporpoises.savingsylvester;
+
+public class RottenPotatoes extends Thing {
+
+	public RottenPotatoes()
+	{
+		name = "potatoes";
+		description = "";
+		collectable = false;
+	}
+}
--- src/com/immortalporpoises/savingsylvester/WestGarden.java
+++ src/com/immortalporpoises/savingsylvester/WestGarden.java
@@ -7,6 +7,8 @@ public class WestGarden extends Environment {
 		environ_name = "garden";
 		entry_description = "Upon arriving in this area of the garden, you are immediately overcome with the stench"
 				+ " of rotting potatoes.";
-		main_description = "The gardener has apparently been hard at work in this area of the garden";
+		main_description = "The gardener has apparently been hard at work in this area of the garden. A pile of "
+				+ "rotten potatoes lies in a mound beside a garden bed. A shovel and wheelbarrow rest next to the"
+				+ " pile.";
 	}
 }
