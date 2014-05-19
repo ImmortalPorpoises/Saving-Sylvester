36dfbb0a1660a76f209870a76f40e8784761bb81
jasterph <godisblackee@gmail.com>
2014-05-19 18:01:12 -0500

    sword add

--- src/com/immortalporpoises/savingsylvester/Southwestgarden.java
+++ src/com/immortalporpoises/savingsylvester/Southwestgarden.java
@@ -5,13 +5,17 @@ public class Southwestgarden extends Environment
 
 	public Southwestgarden() 
 	{
-		environ_name = "garden";//name of envioment
-		entry_description = "When you enter the south west coner of the garden you notice a rather large" + " knight polishing a" + " sword.";// decription of the environmanet
-		main_description = "affa";//look commant
-		
-		//setup
-		Knight k = new Knight();
-		sthings_in_environ.add(k);
+		{
+			environ_name = "garden";//name of envioment
+			entry_description = "When you enter the south west coner of the garden you notice a rather large" + " knight polishing a" + " sword.";// decription of the environmanet
+			main_description = "affa";//look command
+			
+			//setup
+			Knight k = new Knight();
+			sthings_in_environ.add(k);
+			Sword s = new Sword();
+			sthings_in_environ.add(s);
+		}
 	}
 
 }
